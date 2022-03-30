02f5266cdcf2afe678ad8f81303e6904c43a90ba
Alec <Alexranjitkar@gmail.com>
2022-03-30 15:24:23 +0200

    AceSearch

--- src/Search.java
+++ src/Search.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Search {
 
     private final Board board;
@@ -8,5 +11,25 @@ public class Search {
 
     public void doShit() {
         System.out.println(board.toString());
+    }
+
+    public List<Object> aceSearch (){
+
+        int aceIndex = 0;
+        for (int i = 1; i <=7 ; i++) {
+
+            CardDeck sourceDeck = this.board.getDeck(Integer.toString(i));
+            int sourceTopCardIndex = sourceDeck.getBottomFaceCardIndex();
+            Card sourceTopCard = sourceDeck.get(sourceTopCardIndex);
+
+            if (sourceTopCard.getValue()== 1){
+                aceIndex = sourceTopCardIndex;
+
+            }
+
+
+        }
+        return Arrays.asList(sourceDeck, aceIndex);
+
     }
 }
