b57667f1d4590617921ff5732a3bdf4ae37ca4aa
Ayafahim <bvf-2002@live.dk>
2022-06-04 00:46:05 +0200

    havde glemt at committe mine kommentarer

--- src/Search.java
+++ src/Search.java
@@ -42,6 +42,10 @@ public class Search {
         System.out.println(board.toString());
     }
 
+    /** Author Aya
+     Searches for revealed aces and returns pile number, row number (bottom = 1), suit)
+     */
+
     public List<Object> aceSearch() {
 
         for (int i = 1; i <= 7; i++) {
@@ -50,6 +54,51 @@ public class Search {
             Card sourceTopCard = sourceDeck.get(sourceTopCardIndex);
 
             if (sourceTopCard.getValue() == 1) {
+                switch (sourceTopCard.getSuit()) {
+                    case HEARTS -> destination = board.heartsPile;
+                    case SPADES -> destination = board.spadesPile;
+                    case DIAMONDS -> destination = board.diamondsPile;
+                    case CLUBS -> destination = board.clubsPile;
+                    default -> destination = board.initialDeck;
+                }
+                setDeckNumber(i);
+                setAceIndex(sourceTopCard.getValue());
+                break;
+            }
+        }
+        if (deckNumber == 0 && aceIndex == 0) {
+            for (int i = 0; i <= this.board.drawDeck.size(); i++) {
+                CardDeck sourceDeck = this.board.drawDeck;
+                Card card = sourceDeck.get(i);
+                if (card.getValue() == 1) {
+                    switch (card.getSuit()) {
+                        case HEARTS -> destination = board.heartsPile;
+                        case SPADES -> destination = board.spadesPile;
+                        case DIAMONDS -> destination = board.diamondsPile;
+                        case CLUBS -> destination = board.clubsPile;
+                        default -> destination = board.initialDeck;
+                    }
+                    setDeckNumber(12);
+                    setAceIndex(i);
+                    break;
+                }
+            }
+
+        }
+        return Arrays.asList(deckNumber, aceIndex, destination.getName());
+    }
+
+    /** Author Aya
+     Searches for revealed deuce and returns pile number, row number (bottom = 1), suit)
+     */
+    public List<Object> deuceSearch() {
+
+        for (int i = 1; i <= 7; i++) {
+            CardDeck sourceDeck = this.board.getDeck(Integer.toString(i));
+            int sourceTopCardIndex = sourceDeck.getBottomFaceCardIndex();
+            Card sourceTopCard = sourceDeck.get(sourceTopCardIndex);
+
+            if (sourceTopCard.getValue() == 2) {
                 switch (sourceTopCard.getSuit()) {
                     case HEARTS -> destination = board.heartsPile;
                     case SPADES -> destination = board.spadesPile;
