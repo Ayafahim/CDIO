8eae87c7b444dc4c89f5bea847ee515b92a1fd2e
Ayafahim <bvf-2002@live.dk>
2022-06-09 20:24:32 +0200

    small fixes and comments added to search

--- src/Search.java
+++ src/Search.java
@@ -17,8 +17,10 @@ public class Search {
     public List<Object> someSearch(int cardValue) {
         int cardValueIndex = 0;
         int deckNumber = 0;
-        CardDeck destination = null;
+        String cardSuit = "0";
 
+
+        //for loop iterates through each pile and checks value of each card facing up
         for (int i = 1; i <= 7; i++) {
             CardDeck sourceDeck = this.board.getDeck(Integer.toString(i));
             int sourceTopCardIndex = sourceDeck.getBottomFaceCardIndex();
@@ -26,36 +28,19 @@ public class Search {
 
             if (sourceTopCard.getValue() == cardValue) {
                 switch (sourceTopCard.getSuit()) {
-                    case HEARTS -> destination = board.heartsPile;
-                    case SPADES -> destination = board.spadesPile;
-                    case DIAMONDS -> destination = board.diamondsPile;
-                    case CLUBS -> destination = board.clubsPile;
-                    default -> destination = board.initialPile;
+                    case HEARTS -> cardSuit = "Hearts";
+                    case SPADES -> cardSuit = "Spades";
+                    case DIAMONDS -> cardSuit = "Diamonds";
+                    case CLUBS -> cardSuit = "Clubs";
+                    default -> cardSuit = "0";
                 }
+                //if card of certain value isn't facing up deckNumber and cardValueIndex will be 0
                 deckNumber = i;
                 cardValueIndex = sourceTopCardIndex;
                 break;
             }
         }
-        if (deckNumber == 0 && cardValueIndex == 0) {
-            for (int i = 0; i <= this.board.drawPile.size(); i++) {
-                CardDeck sourceDeck = this.board.drawPile;
-                Card card = sourceDeck.get(i);
-                if (card.getValue() == 1) {
-                    switch (card.getSuit()) {
-                        case HEARTS -> destination = board.heartsPile;
-                        case SPADES -> destination = board.spadesPile;
-                        case DIAMONDS -> destination = board.diamondsPile;
-                        case CLUBS -> destination = board.clubsPile;
-                        default -> destination = board.initialPile;
-                    }
-                    deckNumber = 12;
-                    cardValueIndex = i;
-                    break;
-                }
-            }
-        }
-        return Arrays.asList(deckNumber, cardValueIndex, destination.getName());
+        return Arrays.asList(deckNumber, cardValueIndex, cardSuit);
     }
 
 
